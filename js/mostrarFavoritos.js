992c5a5e083e5d281fb74fe78118b3dee31ac7b6
Eduardo Stein <stein.edu98@gmail.com>
2026-05-31 11:43:27 -0300

    Botao remover dos favoritos

--- js/mostrarFavoritos.js
+++ js/mostrarFavoritos.js
@@ -30,9 +30,27 @@ async function carregarFavoritos() {
                 <p>Temperatura: ${clima.temperature_2m}°${pegarSimboloTemperatura()}</p>
                 <p>Vento: ${clima.wind_speed_10m} km/h</p>
                 <p>Umidade: ${clima.relative_humidity_2m}%</p>
+                <button type="button" onclick="removerFavorito('${cidadeNome}')">
+                    Remover
+                </button>
             </article>
         `;
     }
+}
+
+function removerFavorito(nomeCidade) {
+    const confirmar = confirm(`Deseja realmente remover ${nomeCidade} dos favoritos?`);
+
+    if (!confirmar) {
+        return;
+    }
+
+    const favoritos = pegarFavoritos();
+    const favoritosAtualizados = favoritos.filter(cidade => cidade !== nomeCidade);
+
+    localStorage.setItem("favoritos", JSON.stringify(favoritosAtualizados));
+
+    carregarFavoritos();
 }
 
 carregarFavoritos();
